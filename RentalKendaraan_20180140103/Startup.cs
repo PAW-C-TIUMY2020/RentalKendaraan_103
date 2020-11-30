a0de8fb1152d29898d35c492b979c844517b6706
Rama Soni Ravictor <55401842+ravictor21@users.noreply.github.com>
2020-11-30 22:50:36 +0700

    MEMBUAT FITUR UPLOAD FILE

--- RentalKendaraan_20180140103/Startup.cs
+++ RentalKendaraan_20180140103/Startup.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using RentalKendaraan_20180140103.Models;
 
 namespace RentalKendaraan_20180140103
 {
@@ -25,16 +29,37 @@ namespace RentalKendaraan_20180140103
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IFileProvider>(
+                new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")));
+            
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-            services.AddDbContext<Models.RentKendaraanContext>(options =>
+
+            services.AddDbContext<Models.RentKendaraanContext>(options => 
             options.UseSqlServer(Configuration.GetConnectionString("Default")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultUI()
+                .AddEntityFrameworkStores<RentKendaraanContext>().AddDefaultTokenProviders();
+            services.AddAuthorization(option =>
+            {
+                option.AddPolicy("raadonlypolicy",
+                    builder => builder.RequireRole("Admin", "Manager", "Kasir"));
+                option.AddPolicy("writepolicy",
+                    builder => builder.RequireRole("Admin", "Kasir"));
+                option.AddPolicy("editpolicy",
+                    builder => builder.RequireRole("Admin", "Kasir"));
+                option.AddPolicy("deletepolicy",
+                    builder => builder.RequireRole("Admin", "Kasir"));
+            });
+            services.AddScoped<Peminjaman>();
+            services.AddScoped<Pengembalian>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
